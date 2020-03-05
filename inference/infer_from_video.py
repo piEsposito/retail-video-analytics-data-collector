cec6a03838a5a344658ef5cbe80610a529b64903
piEsposito <piero.skywalker@gmail.com>
2020-03-05 14:17:42 -0300

    encapsuled video analyzer

--- inference/infer_from_video.py
+++ inference/infer_from_video.py
@@ -2,54 +2,77 @@ from inference.inference_general_utils import *
 from inference.inference_plot_neuralnets import *
 import time
 import numpy as np
+import pandas as pd
 
-def infer_from_video(cap, age_net, aff_net, pose_net, face_cascade, face_net):
-    tic = time.time()
-    frame_nbr = 0
-    collected_data = []
-    fps_list = []
+class VideoAnalyzer:
+    """
+    Implements a frame capturar from the webcam 
+    """
+    def __init__(self,
+                 cap,
+                 age_net,
+                 aff_net,
+                 pose_net,
+                 face_net,
+                 face_cascade=None,):
 
-    face_analyzer = FaceAnalyzer(age_net,
-                                 aff_net, 
-                                 pose_net)
-    while(True):
-        # Capture frame-by-frame
-        video = cap.read()
-        ret, frame = video
-        #print(frame.shape)
-        #detect faces in gray frame, so it runs faster 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        faces = face_net.find_faces(frame)
-        #infere age, gender and expression and label the image
+        self.cap = cap
+        self.face_net = face_net
+        self.face_analyzer = FaceAnalyzer(age_net,
+                                          aff_net, 
+                                          pose_net,)
+        self.collected_data = []
+        self.frame_nbr = 0
+
+    def analyze_video(self):
+        tic = time.time()
+        fps_list = []
+        while(True):
+            video = self.cap.read()
+            ret, frame = video
+            #print(frame.shape)
+            #detect faces in gray frame, so it runs faster 
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            self.analyze_frame(frame, gray)
+            cv2.imshow('frame',frame)
+
+            tac = time.time()
+            tictac = tac - tic
+            tic = tac
+            fps = 1/tictac
+            fps_list.append(fps)
+            print("Frames per second: {0}\r".format(fps), end="\r")
+            self.frame_nbr += 1
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("")
+                break
+
+            # When everything done, release the capture
+
+        print("\n[INFO] - Ending recording and releasing caputre from webcam")
+        self.cap.release()
+        cv2.destroyAllWindows()
+        fps_arr = np.array(fps_list)
+        print("[INFO] - FPS MEAN: ", str(np.mean(fps_arr)))
+        print("[INFO] - FPS STD: ", str(np.std(fps_arr)))
+
+    def analyze_frame(self, frame, gray):
+        """
+        analizes and anotates the frame
+        """
+        faces = self.face_net.find_faces(frame)
         for face in faces:
-            #print(face)
-            ((age, gender), aff_label), (yaw, pitch, roll) = face_analyzer.infere_from_face(frame, gray, face)
+            ((age, gender), aff_label), (yaw, pitch, roll) = self.face_analyzer.infere_from_face(frame, gray, face)
+            self.collected_data.append((self.frame_nbr, age, gender, aff_label, yaw, pitch, roll))
 
-            collected_data.append((frame_nbr, age, gender, aff_label, yaw, pitch, roll))
-
-        # Display the resulting frame
-        cv2.imshow('frame',frame)
-
-        #to get the fps
-        tac = time.time()
-        tictac = tac - tic
-        tic = tac
-        fps = 1/tictac
-        fps_list.append(fps)
-
-        print("Frames per second: {0}\r".format(fps))
-        frame_nbr += 1
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    print("\n[INFO] - Ending recording and releasing caputre from webcam")
-    cap.release()
-    cv2.destroyAllWindows()
-    fps_arr = np.array(fps_list)
-    print("\n[INFO] - FPS MEAN: ", str(np.mean(fps_arr)))
-    print("[INFO] - FPS STD: ", str(np.std(fps_arr)))
-    return collected_data
-
+    def store_gathered_data(self,
+                            output_path="video_data.csv"):
+        """
+        Stores the data from the captured frames
+        """
+        as_df = pd.DataFrame(self.collected_data)
+        as_df.columns = ["frame", "age", "gender", "emotion", "yaw", "pitch", "roll"]
+        print("\n[INFO] - saving raw collected data to csv in in path:", output_path)
+        as_df.to_csv(output_path)
+        return as_df 
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import argparse
 from inference.inference_general_utils import *
 from inference.inference_plot_neuralnets import *
 from inference.infer_from_video import *
-from inference.data_saver_utils import *
 
 
 
@@ -28,14 +27,11 @@ ap.add_argument('-facede', '--face_net_device', required=False, default="CPU",
 		help="device to plugin the OpenVINO optimized pose_net neural network: MOVIDIUS, CPU, GPU")
 
 args = vars(ap.parse_args())
-#print(args)
-#capture video from the webcam of index 0
 cap = cv2.VideoCapture(0)
 
 #here we set up our neural networks and face classifiers 
 age_net_bin = "inference/neuralnets/age-gender-recognition-retail-0013.bin"
 age_net_xml = "inference/neuralnets//age-gender-recognition-retail-0013.xml"
-#age_net = load_net(age_net_xml, age_net_bin, device=args["age_net_device"])
 age_net = OptimizedNN(age_net_xml,
 					  age_net_bin,
 					  blob_shape=(62,62),
@@ -45,7 +41,6 @@ age_net = OptimizedNN(age_net_xml,
 
 aff_net_bin = "inference/neuralnets//emotions-recognition-retail-0003.bin"
 aff_net_xml = "inference/neuralnets//emotions-recognition-retail-0003.xml"
-#aff_net = load_net(aff_net_xml, aff_net_bin, device=args["aff_net_device"])
 aff_net = OptimizedNN(aff_net_xml,
 					  aff_net_bin,
 					  blob_shape=(64,64),
@@ -55,7 +50,6 @@ aff_net = OptimizedNN(aff_net_xml,
 
 pose_net_bin = "inference/neuralnets//head-pose-estimation-adas-0001.bin"
 pose_net_xml = "inference/neuralnets//head-pose-estimation-adas-0001.xml"
-#pose_net = load_net(pose_net_xml, pose_net_bin, device=args["pose_net_device"])
 pose_net = OptimizedNN(pose_net_xml,
 		 			   pose_net_bin,
 		 			   blob_shape=(60,60),
@@ -65,7 +59,6 @@ pose_net = OptimizedNN(pose_net_xml,
 
 face_net_bin = "inference/neuralnets//face-detection-retail-0004.bin"
 face_net_xml = "inference/neuralnets//face-detection-retail-0004.xml"
-#face_net = load_net(face_net_xml, face_net_bin, device=args["face_net_device"])
 face_net = MultiBoxNN(face_net_xml,
 					   face_net_bin,
 					   blob_shape=(300, 300),
@@ -73,9 +66,13 @@ face_net = MultiBoxNN(face_net_xml,
 					   device=args["pose_net_device"],
 					   swapRB=True)
 
-face_cascade = cv2.CascadeClassifier('inference/neuralnets/haarcascade_frontalface_default.xml')
+#face_cascade = cv2.CascadeClassifier('inference/neuralnets/haarcascade_frontalface_default.xml')
 
-#here we run the program
-data = infer_from_video(cap, age_net, aff_net, pose_net, face_cascade, face_net)
-df = store_gathered_data(data, output_path=args["output_raw_data_path"])
-df_hoted = one_hot_encode_gathered_data(df, output_path=args["output_preprocessed_data_path"])
+videoanalyzer = VideoAnalyzer(cap,
+							  age_net,
+							  aff_net,
+							  pose_net,
+							  face_net,
+							  face_cascade=None,)
+videoanalyzer.analyze_video()
+videoanalyzer.store_gathered_data(output_path=args["output_raw_data_path"])
